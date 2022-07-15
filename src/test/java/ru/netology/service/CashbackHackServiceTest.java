b8b2a7e7e986cbe526ff4d9c29742c3cf0d76416
Азат Ильдарханов <defcon797@gmail.com>
2022-07-15 19:55:55 +0300

    fix on assertEquals mixed expected and actual

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -15,7 +15,7 @@ public class CashbackHackServiceTest {
         int actual = cashbackHackService.remain(amount);
         int expected = 100;
 
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
     @Test
@@ -27,7 +27,7 @@ public class CashbackHackServiceTest {
         int actual = cashbackHackService.remain(amount);
         int expected = 0;
 
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
     @Test
@@ -39,6 +39,6 @@ public class CashbackHackServiceTest {
         int actual = cashbackHackService.remain(amount);
         int expected = 600;
 
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 }
